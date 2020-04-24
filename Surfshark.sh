98ee327ddc35134ba2b96ec59431f1b711d72a2e
David Bauch <djbii@Davids-MacBook-Pro.local>
2020-04-24 13:46:57 -0600

    remove Surfshark.sh

--- Surfshark.sh
+++
@@ -1,4 +0,0 @@
-sed -i -e '58s/false/true/' /Applications/Surfshark.app/Contents/Info.plist
-open /Applications/Surfshark.app
-sed -i -e '58s/true/false/' /Applications/Surfshark.app/Contents/Info.plist
-exit
