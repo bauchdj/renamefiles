467e93f45553fc8a6858273eb1596d1ab9a2feda
David Bauch <djbii@davids-mbp.lan>
2020-06-30 01:36:11 -0500

    added to Ma's pic year/month folder management script

--- Mayearfolders.sh
+++ Mayearfolders.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
-count=0
-date="2020"
-mkdir $date &> /dev/null
-for d in *$date; do
-	if ! [ "$d" == "$date" ]; then
-		count=$(expr $count + 1)
-		#echo "$d" "|" "$date" "|" "$count"
-		mv "$d" "$date"
-  fi
-done
 
+#date="2020"
+#mkdir $date &> /dev/null
+#for d in *$date; do
+	#if ! [ "$d" == "$date" ]; then
+		#mv "$d" "$date"
+  #fi
+#done
+
+monthloop () {
+for m in ${months[@]}; do
+	SUB=$m
+	if [[ "$d" == *"$SUB"* ]]; then
+		mkdir "$SUB" &> /dev/null
+		if ! [ "$d" == "$SUB" ]; then
+			mv "$d" "$SUB"
+		fi
+	fi
+done
+}
+
+months=("January" "February" "March" "April" "May" "June" "July" "August" "September" "October" "November" "December")
+for d in *; do
+	monthloop
+done
 exit
