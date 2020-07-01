ee0c61b6b1023ca930700fae9f00ce1eb8d8045b
David Bauch <djbii@Davids-MacBook-Pro.local>
2020-06-30 19:44:49 -0500

    made Mayearfolders.sh cleaner

--- Mayearfolders.sh
+++ Mayearfolders.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#date="2020"
+#date="2013"
 #mkdir $date &> /dev/null
 #for d in *$date; do
 	#if ! [ "$d" == "$date" ]; then
@@ -8,20 +8,30 @@
   #fi
 #done
 
+date=$(echo $PWD | sed 's/.*\(....\)/\1/') #only works for standard date directory
+months=("January" "February" "March" "April" "May" "June" "July" "August" "September" "October" "November" "December")
+nummonths=("01-January" "02-February" "03-March" "04-April" "05-May" "06-June" "07-July" "08-August" "09-September" "10-October" "11-November" "12-December")
+
+movefiles () {
+folder=$(echo ${nummonths[i]} $date)
+mkdir "$folder" &> /dev/null
+if ! [ "$d" == "$folder" ]; then
+	mv "$d" "$folder"
+fi
+}
+
 monthloop () {
-for m in ${months[@]}; do
-	SUB=$m
+for (( i=0; i<12; i++ ))
+do
+	SUB=${months[i]}
 	if [[ "$d" == *"$SUB"* ]]; then
-		mkdir "$SUB" &> /dev/null
-		if ! [ "$d" == "$SUB" ]; then
-			mv "$d" "$SUB"
-		fi
+		movefiles
 	fi
 done
 }
 
-months=("January" "February" "March" "April" "May" "June" "July" "August" "September" "October" "November" "December")
 for d in *; do
 	monthloop
 done
+
 exit
