4fb67da51862229a08ed17493db068fff6a9cb44
bauchdj <64243465+bauchdj@users.noreply.github.com>
2022-11-10 02:11:08 -0600

    made code better by starting i at 1

--- rename-nikon-photos.js
+++ rename-nikon-photos.js
@@ -3,13 +3,13 @@ const fs = require('fs'),
       folderPath = process.argv[2] //__dirname for file's current directory;
 let files = fs.readdirSync(folderPath);
 
-for (let i=0; i<=files.length-1; i++) {
-  let filename = files[i],
+for (let i=1; i<=files.length; i++) {
+  let filename = files[i-1],
   name = path.parse(filename).name,
   ext = path.parse(filename).ext,
   numfromname = parseInt(name.split('_').pop());
-  if (numfromname > i+1) {
-    let picnumber = (i+1).toString().padStart(4, '0');
+  if (numfromname > i) {
+    let picnumber = (i).toString().padStart(4, '0');
     let newname = `DSC_${picnumber}${ext}`;
     fs.rename(`${folderPath}${filename}`, `${folderPath}${newname}`, () => {
       console.log(filename, newname);
