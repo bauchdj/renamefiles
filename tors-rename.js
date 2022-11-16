395eb931b99140f202c4e9abb56f83d548e3528c
bauchdj <64243465+bauchdj@users.noreply.github.com>
2022-11-16 12:18:49 -0700

    Uncommented rename function

--- tors-rename.js
+++ tors-rename.js
@@ -1,19 +1,15 @@
 const fs = require('fs'),
       path = require('path'),
-      folderPath = process.argv[2] //__dirname for file's current directory;
+      folderPath = process.argv[2] //__dirname current file directory;
 let files = fs.readdirSync(folderPath);
 
 for (let i=0; i<=files.length-1; i++) {
   let filename = files[i],
   name = path.parse(filename).name,
   ext = path.parse(filename).ext,
-  moviename = name.split(/.[0-9][0-9][0-9][0-9]./)[0];
-  moviename = moviename.split('.').join(' ');
-  let newname = `${moviename}${ext}`;
-  console.log(newname);
-  /*
-  fs.rename(`${folderPath}${filename}`, `${folderPath}${newname}`, () => {
-    console.log(filename, newname);
+  moviename = name.split(/.[0-9][0-9][0-9][0-9]./)[0].split('.').join(' ');
+  let newfilename = `${moviename}${ext}`;
+  fs.rename(`${folderPath}${filename}`, `${folderPath}${newfilename}`, () => {
+    console.log(filename, newfilename);
   });
-  */
 }
