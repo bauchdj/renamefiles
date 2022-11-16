9b97ba6c3e6f5deb899e40a3700fc27ddf27651a
bauchdj <64243465+bauchdj@users.noreply.github.com>
2022-11-16 12:40:40 -0700

    Added / logic to folderPath

--- tors-rename.js
+++ tors-rename.js
@@ -1,6 +1,7 @@
 const fs = require('fs'),
       path = require('path'),
       folderPath = process.argv[2] //__dirname current file directory;
+			folderPath = folderPath.replace(/\/?$/, '/');
 let files = fs.readdirSync(folderPath);
 
 for (let i=0; i<=files.length-1; i++) {
@@ -9,7 +10,8 @@ for (let i=0; i<=files.length-1; i++) {
   ext = path.parse(filename).ext,
   moviename = name.split(/.[0-9][0-9][0-9][0-9]./)[0].split('.').join(' ');
   let newfilename = `${moviename}${ext}`;
-  fs.rename(`${folderPath}${filename}`, `${folderPath}${newfilename}`, () => {
+  fs.rename(`${folderPath}${filename}`, `${folderPath}${newfilename}`, (err) => {
+		if (err) throw err;
     console.log(filename, newfilename);
   });
 }
