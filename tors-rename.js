5708b19f14b4f848981206b58720849e8ca5440a
bauchdj <64243465+bauchdj@users.noreply.github.com>
2022-11-16 13:36:27 -0700

    Each tor in sub-folder

--- tors-rename.js
+++ tors-rename.js
@@ -1,17 +1,28 @@
 const fs = require('fs'),
-      path = require('path'),
-      folderPath = process.argv[2] //__dirname current file directory;
-			folderPath = folderPath.replace(/\/?$/, '/');
-let files = fs.readdirSync(folderPath);
+      path = require('path');
+let dir = process.argv[2] //${__dirname} current file directory
+		dir = dir.replace(/\/?$/, '/');
+let folders = fs.readdirSync(dir);
 
-for (let i=0; i<=files.length-1; i++) {
-  let filename = files[i],
-  name = path.parse(filename).name,
-  ext = path.parse(filename).ext,
-  moviename = name.split(/.[0-9][0-9][0-9][0-9]./)[0].split('.').join(' ');
-  let newfilename = `${moviename}${ext}`;
-  fs.rename(`${folderPath}${filename}`, `${folderPath}${newfilename}`, (err) => {
-		if (err) throw err;
-    console.log(filename, newfilename);
-  });
+for (let i=0; i<folders.length; i++) {
+	let folder = `${dir}${folders[i].replace(/\/?$/, '/')}`;
+	//if (fs.statSync(folder).isDirectory) renameTors(fs.readdirSync(folder));
+	renameTors(fs.readdirSync(folder),folder);
+}
+
+function renameTors(files,folderPath) {
+	for (let i=0; i<files.length; i++) {
+	  let filename = files[i],
+	  name = path.parse(filename).name,
+	  ext = path.parse(filename).ext,
+	  moviename = name.split(/.[0-9][0-9][0-9][0-9]./)[0].split('.').join(' '),
+	  newfilename = `${moviename}${ext}`;
+	 	console.log(`${folderPath}${filename}`, `${folderPath}${newfilename}`);
+	/*
+	 	fs.rename(`${folderPath}${filename}`, `${folderPath}${newfilename}`, (err) => {
+			if (err) throw err;
+ 	  	console.log(filename, newfilename);
+	  });
+	*/
+	}
 }
