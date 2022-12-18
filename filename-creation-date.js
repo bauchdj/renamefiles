8595394732909458e46042cba35415013e94a492
David Bauch <djbauch@Davids-MacBook-Pro.local>
2022-12-18 00:50:05 -0700

    file creation date script changes modification and creation date

--- filename-creation-date.js
+++ filename-creation-date.js
@@ -1,4 +1,5 @@
 const fs = require('fs'), path = require('path');
+const { spawn } = require('child_process');
 let dir = process.argv[2].replace(/\/?$/, '/');
 let files = fs.readdirSync(dir).filter(item => !(/(^|\/)\.[^\/\.]/g).test(item));
 
@@ -6,14 +7,15 @@ for (let i=0; i<files.length; i++) {
         let file = files[i];
 	let regex = /201\d*.\d*/;
 	if (file.match(regex)) {
-	let filedate = (file.match(regex)[0].replace(/[^0-9]/g,''));
-	var year = filedate.substring(0,4);
-	var month = filedate.substring(4,6);
-	var day = filedate.substring(6,8);
-	var hour = filedate.substring(8,10);
-	var min = filedate.substring(10,12);
-	var sec = filedate.substring(12,14);
-	var date = `${month}/${day}/${year} ${hour}:${min}:${sec}`;
-	console.log(file "|" date);
+		var filedate = (file.match(regex)[0].replace(/[^0-9]/g,''));
+		var year = filedate.substring(0,4);
+		var month = filedate.substring(4,6);
+		var day = filedate.substring(6,8);
+		var hour = filedate.substring(8,10);
+		var min = filedate.substring(10,12);
+		var sec = filedate.substring(12,14);
+		var date = `${month}/${day}/${year} ${hour}:${min}:${sec}`;
+		var setfile = spawn('SetFile', ['-d', date, '-m', date, file]);
+		console.log(`${file} | ${date}`);
 	}
 }
